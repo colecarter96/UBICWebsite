f5957adb89e664811f383d51b44bdbb4c26ecfc6
Cole <colescarter@gmail.com>
2024-11-01 17:28:58 -0700

    Fixed back

---
+++ ubic-website/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    console.log("OOOOOOOOOGAAAAABOOOOOOOOOOGAAAAA");
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+         
+    );
+}
+
+export default HomePage;
