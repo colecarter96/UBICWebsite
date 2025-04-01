8d412c68dd7e5b6ba16072deda07e8e44efd4042
Cole <colescarter@gmail.com>
2025-04-01 09:14:14 -0700

    Desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Header({ alwaysBackground }) {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     useEffect(() => {
         if (alwaysBackground) {
@@ -23,19 +24,38 @@ function Header({ alwaysBackground }) {
         return () => window.removeEventListener('scroll', handleScroll);
     }, [alwaysBackground]);
 
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <header className={`header ${isScrolled ? 'scrolled' : ''} ${alwaysBackground ? 'home-fade' : ''}`}>
             <Link to='/' className="myLink">
                 <h1>UBIC at UCSD</h1>
             </Link>
             
-            <nav>
+            <button className="hamburger-menu" onClick={toggleMobileMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            
+            <nav className={`nav-menu ${isMobileMenuOpen ? 'open' : ''}`}>
                 <ul>
-                    <Link to='/events' className="myLink">
+                    <Link to='/about' className="myLink" onClick={() => setIsMobileMenuOpen(false)}>
+                        <li>About</li>
+                    </Link>
+                    <Link to='/bioinformatics' className="myLink" onClick={() => setIsMobileMenuOpen(false)}>
+                        <li>Bioinformatics</li>
+                    </Link>
+                    <Link to='/events' className="myLink" onClick={() => setIsMobileMenuOpen(false)}>
                         <li>Events</li>
                     </Link>
-                    <Link to='/team' className="myLink">
+                    <Link to='/team' className="myLink" onClick={() => setIsMobileMenuOpen(false)}>
                         <li>Team</li>
+                    </Link>
+                    <Link to='/contact' className="myLink" onClick={() => setIsMobileMenuOpen(false)}>
+                        <li>Get Involved</li>
                     </Link>
                 </ul>
             </nav>
