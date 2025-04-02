9fdb235c1075fc0fb5f3752db76d7f0a84f8cb5c
Cole <colescarter@gmail.com>
2025-04-02 16:28:44 -0700

    ESLint

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from '../components/Header';
-import Hero from '../components/Hero';
+// import Hero from '../components/Hero';
 import Footer from '../components/Footer';
-import EventCard from '../components/EventCard';
+// import EventCard from '../components/EventCard';
 import './EventsPage.css';
 import EventCardsContainer from '../components/EventCardsContainer';
 
