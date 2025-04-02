358015bf74ade66635f2744ca960338ab9c7e4f6
Cole <colescarter@gmail.com>
2025-04-02 16:31:41 -0700

    ESLint

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -4,7 +4,7 @@ import './TeamCard.css'
 const TeamCard = ({name, position, imag}) => {
     return (
         <div className='team-card'>
-            <img src={imag} alt="UBIC TEAM IMAGE" loading="lazy"/>
+            <img src={imag} alt={`${name} - ${position}`} loading="lazy"/>
             <h3>{name}</h3>
             <p>{position}</p>
         </div>
