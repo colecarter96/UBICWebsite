f55d71f410fbd64476ef650d94d3d7c9fd8c241c
Cole <colescarter@gmail.com>
2025-05-30 10:09:19 -0700

    Build error

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import AboutSection from '../components/AboutSection'
-import NewsletterSection from '../components/NewsletterSection';
 import Footer from '../components/Footer';
-import EventCardsContainer from '../components/EventCardsContainer';
+
 
 // Sample data - Replace this with actual data from an API or database
 const events = [
