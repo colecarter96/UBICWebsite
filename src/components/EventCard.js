935639b8fe049194df6d6ac853d92e7505a4179a
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2025-09-04 14:13:43 -0700

    Event Cards - Hover for description

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,14 +1,11 @@
-import React from "react";
 import "./EventCard.css"; // Create this file for styling if needed
 
 const EventCard = ({ imgsrc, title, description }) => {
   return (
     <div className="event-card">
       <img src={imgsrc} alt={imgsrc} height="150px"></img>
-      <div>
-        <h2> {title} </h2>
-        <p>{description}</p>
-      </div>
+      <h2> {title} </h2>
+      <p>{description}</p>
     </div>
   );
 };
