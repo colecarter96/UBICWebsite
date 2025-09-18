68f9c409398934a2dd6a3f74338bf6eef1129230
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2025-09-17 18:23:32 -0700

    Font change

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -3,7 +3,7 @@ import "./TeamCard.css";
 const TeamCard = ({ name, position, img, major, year, email }) => {
   return (
     <div className="team-card">
-      <img src={img} alt={`${name} - ${position}`} loading="lazy" />
+      <img src={img} alt={`${name}_photo`} loading="lazy" />
       <div className="div-center">
         <h3>{name}</h3>
         <p>{position}</p>
